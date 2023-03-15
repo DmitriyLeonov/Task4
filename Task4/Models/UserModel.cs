58ac6862c844b38e592cd1d14a47c169a65e01f3
DmitriyLeonov <dleonov701@gmail.com>
2023-03-15 19:47:16 +0300

    Add project files.

---
+++ Task4/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Task4.Models
+{
+    public class UserModel
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+        public DateTime RegistrationDateTime { get; set; }
+        public DateTime LastLogInTime { get; set; }
+
+    }
+}
